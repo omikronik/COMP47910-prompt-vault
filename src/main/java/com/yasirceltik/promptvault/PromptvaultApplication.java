a1cb7b1e2a95be4933b83105b3814e4ad3435fb9
omikronik <yasirceltik9@gmail.com>
2026-06-08 22:40:43 +0100

    add project bootstrap

---
+++ src/main/java/com/yasirceltik/promptvault/PromptvaultApplication.java
@@ -0,0 +1,13 @@
+package com.yasirceltik.promptvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yasirceltik/promptvault/PromptvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasirceltik.promptvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
